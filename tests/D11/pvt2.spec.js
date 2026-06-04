553eddfd0770efd3f80af12ed426b7e84959827b
Venkata Modgu <venkatamodgu@OSB-MACPRO-FOYR-080621.local>
2026-06-04 17:57:16 +1000

    D11 pvt updated

--- tests/D11/pvt2.spec.js
+++ tests/D11/pvt2.spec.js
@@ -4,8 +4,6 @@ import sites from './sites.json';
 const filteredSites = sites.sites.filter((site) => site !== 'gsb');
 
 filteredSites.forEach((site) => {
-  //const siteUrl = `https://nginx-canary-${site}.govcms10.amazee.io`;
-  const siteUrl = `https://nginx-master-${site}.govcms5.amazee.io`;
 
 //test.describe.parallel(`Tests for site: ${site}`, () => {
     // Test home page
@@ -30,51 +28,3 @@ filteredSites.forEach((site) => {
       });
 
     });
-
-    // Test search page
-    test(`Verify site search page: ${site}`, async ({ page }, testInfo) => {
-      const response = await page.goto(siteUrl + '/search?keys=test');
-      
-      if (response) {
-        const status = response.status();
-
-        if (status === 401) {
-          console.warn(`Shield detected for site: ${site} - Skipping visual test.`);
-        }
-
-        // expect(status).toBe(200); 
-
-        await page.screenshot({
-        path: testInfo.outputPath(`Search page ${site}.png`),
-        fullPage: true,
-      });
-      } else {
-        console.log(`Test failed: ${site} - No response received.`);
-      }
-    });
-
-    // Test user login page
-    test(`Verify site user login page: ${site}`, async ({ page }, testInfo) => {
-      const response = await page.goto(siteUrl + '/user');
-      
-      if (response) {
-        const status = response.status();
-
-        if (status === 401) {
-          console.warn(`Shield detected for site: ${site} - Skipping visual test.`);
-        }
-
-        // expect(status).toBe(200); 
-
-        await page.screenshot({
-        path: testInfo.outputPath(`Login page ${site}.png`),
-        fullPage: true,
-      });
-      } else {
-        console.log(`Test failed: ${site} - No response received.`);
-      }
-    });
-
-  });
-
-//});
